873b62382e027dd1b9c761a634952a397891d598
Patrick Coral <patrick9999@hotmail.ch>
2023-04-05 20:22:00 +0200

    Added test data, css

--- src/app/classes/line.ts
+++ src/app/classes/line.ts
@@ -1,29 +1,28 @@
 abstract class Line {
-    name: string = '';
-    total: number = 0;
+	name: string = '';
+	total: number = 0;
 }
 
-enum lineType {
-    Group,
-    Entry
-}
+export type lineType
+	= Group
+	| Entry
 
 export class Group extends Line {
-    children: Map<string, lineType> = new Map();
+	children: Map<string, lineType> = new Map();
 
-    public constructor(name: string) {
-        super();
-        this.name = name;
-    }
+	public constructor(name: string) {
+		super();
+		this.name = name;
+	}
 }
 
 export class Entry extends Line {
-    quantity: number = 0;
-    unit: string = '';
-    unitPrice: number = 0;
+	quantity: number = 0;
+	unit: string = '';
+	unitPrice: number = 0;
 
-    public constructor(name: string) {
-        super();
-        this.name = name;
-    }
+	public constructor(name: string) {
+		super();
+		this.name = name;
+	}
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { Group } from '../classes/line';
+import { Entry, Group, lineType } from '../classes/line';
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+	selector: 'app-table',
+	templateUrl: './table.component.html',
+	styleUrls: ['./table.component.css']
 })
-export class TableComponent {
-  displayedColumns = ['name', 'quantity', 'unit', 'unitPrice', 'total'];
+export class TableComponent implements OnInit {
+	displayedColumns = ['name', 'quantity', 'unit', 'unitPrice', 'total'];
 
-  list: Group[] = [new Group('Installation chauffage')];
-  source: MatTableDataSource<Group> = new MatTableDataSource(this.list);
+	list: lineType[] = [new Group('Installation chauffage')];
+	source: MatTableDataSource<lineType> = new MatTableDataSource(this.list);
+
+	//test data
+	ngOnInit() {
+		if (this.list[0] instanceof Group) {
+			this.list[0].children.set('asdf', new Entry('asdf'));
+		}
+		for (let x = 0; x < 10; x++) {
+			this.list.push(new Group(x.toString()));
+		}
+
+		this.source = new MatTableDataSource(this.list);
+	}
 }
